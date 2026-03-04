084546d7f89ee02ffc4309559d4b81bd3e04729c
Neil Patrick Mulingbayan <neilpatrick.personal@gmail.com>
2026-03-05 07:21:31 +0800

    fix

--- app/Models/Challenge.php
+++ app/Models/Challenge.php
@@ -3,8 +3,20 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Challenge extends Model
 {
-    //
+    protected $guarded = [];
+
+    public function task(): BelongsTo
+    {
+        return $this->belongsTo(Task::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
 }
